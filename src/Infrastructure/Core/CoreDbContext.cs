f8fd097e493d7f26690ad3a8f850e9f93f67cf44
Pavlo Panko <pavlikosar@gmail.com>
2025-01-29 20:33:28 +0200

    Add database sets

--- src/Infrastructure/Core/CoreDbContext.cs
+++ src/Infrastructure/Core/CoreDbContext.cs
@@ -1,7 +1,22 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Core;
 
 internal sealed class CoreDbContext(DbContextOptions<CoreDbContext> options) : DbContext(options)
 {
+	#region Tabels
+	public DbSet<Eatery> Eateries { get; set; }
+	public DbSet<Menu> Menus { get; set; }
+	public DbSet<MenuItem> MenuItems { get; set; }
+	#endregion
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+		modelBuilder.Entity<MenuItem>()
+			.Property(p => p.Currency)
+			.HasConversion<string>();
+	}
 }
